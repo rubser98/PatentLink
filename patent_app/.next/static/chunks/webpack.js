b3a5f1fb2bcf27680024851c6147ec1ebfbee976
Paolo Marchignoli <paolo.marchignoli2000@gmail.com>
2024-01-18 17:28:15 +0100

    nuova versione pagine di buy

--- patent_app/.next/static/chunks/webpack.js
+++ patent_app/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "6e745cde0bbc8bc8"; }
+/******/ 		__webpack_require__.h = function() { return "65d1ac3072b2a9e4"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
